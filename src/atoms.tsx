3f177d8e9b146714500a0f374a490bbf083191c1
Harry Jang <harry.jang@kakao.com>
2023-09-12 23:50:55 +0900

    트렐로 프로젝트로 전환을 위한 프로젝트 초기화

--- src/atoms.tsx
+++ src/atoms.tsx
@@ -1,44 +1,14 @@
 import { atom, selector } from "recoil";
-import { recoilPersist } from "recoil-persist";
 
-const { persistAtom } = recoilPersist();
-    
-export interface IToDo {
-    text:string;
-    id: number;
-    category:string;
-}
-
-export enum DefaultCategories  {
-    "TO_DO" = "TO_DO",
-    "DOING" = "DOING",
-    "DONE" = "DONE",
-}
-
-export const categories = atom<string[]>( {
-    key : "customCategory",
-    default: [],
-    effects_UNSTABLE: [persistAtom],
+export const minuteState = atom({
+    key: "minutes",
+    default: 0,
 });
 
-export const categoryState = atom<string>({
-    key: "category",
-    "default":"TO_DO",
-})
-
-export const toDoState = atom<IToDo[]>({
-    key:"toDo",
-    default:[],
-    effects_UNSTABLE: [persistAtom],
-});
-
-export const toDoSelector = selector({
-    key:"toDoSelector", 
+export const hourSelector = selector({
+    key: "hours",
     get: ({get}) => {
-        const toDos = get(toDoState);
-        const category = get(categoryState);
-        
-        return toDos.filter((toDo => toDo.category === category));
+        const minutes = get(minuteState);
+        return minutes / 60;
     },
-})
-
+});
